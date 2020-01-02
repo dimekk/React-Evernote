e41688ef324503053d51e03b78a502b80bb9e825
Wojciech Burczyk <wojciech.burczyk@hotelgram.pl>
2020-01-02 22:09:03 +0100

    setting editor component

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -5,3 +5,16 @@ import debounce from '../helpers';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
+
+class EditorComponent extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>hello from the editor</div>
+        )
+    }
+}
+
+export default withStyles(styles)(EditorComponent);
