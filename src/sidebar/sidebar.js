ea8b42eaa5d0131ce283190beaa8f7f6e22076fd
Wojciech Burczyk <wojciech.burczyk@hotelgram.pl>
2020-01-02 22:12:00 +0100

    setting sidebar component

---
+++ src/sidebar/sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import List from '@material-ui/core/List';
+import { Divider, Button } from '@material-ui/core';
+import SidebarItemComponent from '../sidebaritem/sidebarItem';
+
+class SiderBarComponent extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                hello from sidebar
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(SidebarItemComponent);
